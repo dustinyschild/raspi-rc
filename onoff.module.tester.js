4a9d212c877db81cb44c302140627a670c786505
Dustin <dyelland@gmail.com>
2017-10-16 02:21:44 -0500

    testing if the node event listeners pick up keypress keyword

--- onoff.module.tester.js
+++ onoff.module.tester.js
@@ -1,15 +1,16 @@
+const EventEmitter = require('events');
 const onoff = require('onoff');
 
 const gpio = onoff.Gpio;
 const motor = new gpio(23,'out');
+const myEmitter = new EventEmitter();
 
-const interval = setInterval(function(){
+myEmitter.once('keypress',function(){
   let value = (motor.readSync() + 1) % 2;
   motor.write(value,() => console.log(`Changed State: ${value}`));
 },2000);
 
 process.on('SIGINT',() => {
-  clearInterval(interval);
   motor.writeSync(0);
   motor.unexport();
   console.log('exiting...');

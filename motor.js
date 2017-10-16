c07825fa82566daf0cd707d25352419ca8e471da
dustinyschild <dyelland@gmail.com>
2017-10-16 00:43:15 -0500

    chnaged root file in package json and added git ignore file

--- motor.js
+++ motor.js
@@ -4,7 +4,5 @@ const gpio = require('pi-gpio');
 
 //open the pins
 gpio.open(16,'output',function(){
-  gpio.write(16,1,function(){
-    gpio.close(16);
-  });
+  gpio.write(16,1,gpio.close(16));
 });
